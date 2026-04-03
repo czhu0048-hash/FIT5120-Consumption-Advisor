0b78a389db6ef5f87652365dbb04d6205d84bb31
czhu0048 <czhu0048@student.monash.edu>
2026-04-03 19:17:37 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+// import { useRouter } from 'vue-router';
+
+// const router = useRouter();
+
+// const toHome = () => {
+//   router.push("/")
+// }
+</script>
+
+<template>
+  <div class="app-wrapper d-flex flex-row flex-md-column min-vh-100">
+    <nav class="nav-sidebar flex-shrink-0" style="background-color:darkgrey;">
+      <div class="row align-items-center justify-content-center">
+        <label style="font-size: 150%; text-align: center;">Consumption Advisor</label>
+      </div>
+      <ul class="col-5 d-flex flex-column flex-md-row p-2 m-0">
+        <li>
+          <router-link to="/" class="nav_item_custom text-black d-block p-2">Dashboard</router-link>
+        </li>
+        <li>
+          <router-link to="/food" class="nav_item_custom text-black d-block p-2">Food Guidance</router-link>
+        </li>
+        <li>
+          <router-link to="/household" class="nav_item_custom text-black d-block p-2">Household</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <main class="flex-grow-1">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.nav_item_custom {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+/* Movile nav bar */
+.nav-sidebar {
+  width: 120px;
+  min-height: 100vh;
+}
+
+/* Desktop nav bar */
+@media (min-width: 768px) {
+  .nav-sidebar {
+    width: 100%;
+    min-height: auto;
+  }
+
+}
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-12 col-md-6">
+        <h1>Start a healthy and responsible consumption journey from here</h1>
+    </div>
+    <div class="row">
+        <div class="left col-12 col-md-6 d-flex flex-column align-items-center">
+            <!-- A gap used to seperate our main buttons from the welcoming message -->
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="d-flex flex-column align-items-center gap-5">
+                <button class="btn btn-primary">btn1</button>
+                <button class="btn btn-primary">btn2</button>
+                <button class="btn btn-primary">btn3</button>
+            </div>
+        </div>
+        <div class="right col-12 col-md-6">
+            <img src="../assets/TitleImage.png" alt="Consumption Advisor Title Image" sizes="10%" class="img-fluid">
+        </div>
+    </div>
+</template>
+
+<style>
+/* Left half of the dashboard */
+.left {
+    height: 100vh;
+}
+
+/* Right half of the dashboard */
+.right {
+    height: 100vh;
+}
+</style>
