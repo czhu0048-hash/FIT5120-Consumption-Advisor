a353db94cc8eb09be587e49d3b3c2e536a99c189
AnnaKwokCY <ckwo0016@student.monash.edu>
2026-04-16 22:13:05 +1000

    Added icon to nagivation bar

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
   <div class="app-wrapper d-flex flex-row flex-md-column min-vh-100">
     <nav class="nav-sidebar flex-shrink-0 bg_main">
       <div class="row align-items-center justify-content-center">
-        <label class="font_main" style="font-size: 150%; text-align: center;">RedUse</label>
+          <div class="d-flex align-items-center justify-content-center gap-2">
+            <img src="@/assets/Icon_reduse.png" alt="RedUse Logo" class="nav_logo" />
+            <span class="font_main" style="font-size: 150%; text-align: center;">RedUse</span>
+          </div>
       </div>
       <ul class="w-100 d-flex flex-column flex-md-row p-2 m-0" style="align-items: center;">
         <li class="m-2 col-12 col-md-auto" style="text-align: center;">
@@ -16,7 +19,6 @@
           <router-link to="/food" class="nav_item_custom d-block p-2 button_sub">Food
             Guidance</router-link>
         </li>
-
       </ul>
     </nav>
 
@@ -60,6 +62,13 @@
 ul {
   list-style: none;
 }
+
+.nav_logo {
+  height: 2em; 
+  width: auto;
+  display: block;
+}
+
 
 /* Movile nav bar */
 .nav-sidebar {
