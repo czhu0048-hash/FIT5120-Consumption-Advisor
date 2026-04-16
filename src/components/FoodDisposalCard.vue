70ed7acc91af7c7a1128bda5976b7b879118412c
czhu0048 <czhu0048@student.monash.edu>
2026-04-16 17:51:31 +1000

    chore: Used a more visually appealing font to replace the default font

--- src/components/FoodDisposalCard.vue
+++ src/components/FoodDisposalCard.vue
@@ -3,7 +3,7 @@
         <div class="card-header d-flex justify-content-between align-items-start gap-2">
             <span class="fw-bold">{{ method.label }}</span>
             <a v-if="method.source_link" :href="method.source_link" target="_blank" rel="noopener"
-                class="text-nowrap small">External link</a>
+                class="text-nowrap small">Learn more (external link)</a>
         </div>
         <div class="card-body d-flex flex-column gap-2">
             <div>
