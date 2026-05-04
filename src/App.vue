db7e6529e3720f759032217953fc54a9718f28d1
czhu0048 <czhu0048@student.monash.edu>
2026-05-05 09:25:39 +1000

    chore: Changed passwordTemp once more

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ const router = useRouter()
 const go = (path) => router.push(path)
 const passwordCorrect = ref(false);
 const userInput = ref("");
-const passwordTemp = "aNyThInG1sPaSsWoRd";
+const passwordTemp = "Fit5120@Ta38";
 const passwordMessage = ref("")
 
 const navItems = [
